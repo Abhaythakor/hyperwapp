f5f49b463257b9cfd8b7c16d1c92e7792f8d3d6e
Abhaythakor <abhaykhant2002@gmail.com>
2026-03-18 16:04:07 +0530

    release: v0.0.14 - export PEM-encoded CA certificate for proxy trust

--- input/proxy/proxy.go
+++ input/proxy/proxy.go
@@ -1,10 +1,11 @@
 package proxy
-
 import (
 	"bytes"
+	"encoding/pem"
 	"io"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Abhaythakor/hyperwapp/model"
 	"github.com/Abhaythakor/hyperwapp/util"
@@ -16,9 +17,19 @@ import (
 func StartProxy(addr string, outputCh chan<- model.OfflineInput) error {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = false
-	
+
 	// Silence the internal goproxy logger to prevent terminal clutter
 	proxy.Logger = log.New(io.Discard, "", 0)
+
+	// Save the CA certificate (PEM format) to a file so the user can trust it in their browser
+	caCertPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: goproxy.GoproxyCa.Certificate[0],
+	})
+	if err := os.WriteFile("hyperwapp-ca.crt", caCertPEM, 0644); err == nil {
+		util.Info("Security Certificate (PEM) saved to: hyperwapp-ca.crt")
+		util.Info("IMPORT this file into your browser (Authorities/Trusted Root) to fix SSL/Private errors.")
+	}
 
 	// Enable MITM for all HTTPS traffic to intercept headers/body
 	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
